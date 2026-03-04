884402eeba487cce1c5fd2f9c4e7c5f748efcc6b
AnmolPatel20 <anmolkpatel42@gmail.com>
2026-03-04 10:08:46 +0530

    LC61

---
+++ 61_Rotate_List.py
@@ -0,0 +1,55 @@
+"""
+Given the head of a linked list, rotate the list to the right by k places.
+
+ 
+
+Example 1:
+
+
+Input: head = [1,2,3,4,5], k = 2
+Output: [4,5,1,2,3]
+Example 2:
+
+
+Input: head = [0,1,2], k = 4
+Output: [2,0,1]
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 500].
+-100 <= Node.val <= 100
+0 <= k <= 2 * 109
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        if not head:
+            return head
+
+        length = 1
+        tail = head
+
+        while tail.next:
+            tail = tail.next
+            length += 1
+        
+        k = k % length
+        if k == 0:
+            return head
+        
+        cur = head
+        for i in range(length - k -1):
+            cur = cur.next
+        newHead = cur.next
+        cur.next = None
+        tail.next = head
+        return newHead
+
+        
