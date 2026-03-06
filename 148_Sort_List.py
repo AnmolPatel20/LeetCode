392cde93ecad0d237a23b2c6078b263ea67966e8
AnmolPatel20 <anmolkpatel42@gmail.com>
2026-03-06 12:22:17 +0530

    LC148

---
+++ 148_Sort_List.py
@@ -0,0 +1,76 @@
+"""
+Given the head of a linked list, return the list after sorting it in ascending order.
+
+ 
+
+Example 1:
+
+
+Input: head = [4,2,1,3]
+Output: [1,2,3,4]
+Example 2:
+
+
+Input: head = [-1,5,3,4,0]
+Output: [-1,0,3,4,5]
+Example 3:
+
+Input: head = []
+Output: []
+ 
+
+Constraints:
+
+The number of nodes in the list is in the range [0, 5 * 104].
+-105 <= Node.val <= 105
+ 
+
+Follow up: Can you sort the linked list in O(n logn) time and O(1) memory (i.e. constant space)?
+
+"""
+
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head or not head.next:
+            return head
+        
+        #split list into two halfs
+        left = head
+        right = self.getMid(head)
+        temp = right.next
+        right.next = None
+        right = temp
+
+        left = self.sortList(left)
+        right = self.sortList(right)
+        return self.merge(left,right)
+
+    def getMid(self,head):
+        slow , fast = head , head.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+    
+    def merge(self, list1 , list2):
+        dummy = ListNode()
+        tail = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1 = list1.next
+            else:
+                tail.next = list2
+                list2 = list2.next
+            tail = tail.next
+        if list1:
+            tail.next = list1
+        if list2:
+            tail.next = list2
+        
+        return dummy.next
