a0b3495060d3190036c8607b079b1bc24da18fa8
AnmolPatel20 <anmolkpatel42@gmail.com>
2026-04-11 23:16:54 +0530

    LC322

---
+++ 322_Coin_Change.py
@@ -0,0 +1,45 @@
+"""
+You are given an integer array coins representing coins of different denominations and an integer amount representing a total amount of money.
+
+Return the fewest number of coins that you need to make up that amount. If that amount of money cannot be made up by any combination of the coins, return -1.
+
+You may assume that you have an infinite number of each kind of coin.
+
+ 
+
+Example 1:
+
+Input: coins = [1,2,5], amount = 11
+Output: 3
+Explanation: 11 = 5 + 5 + 1
+Example 2:
+
+Input: coins = [2], amount = 3
+Output: -1
+Example 3:
+
+Input: coins = [1], amount = 0
+Output: 0
+ 
+
+Constraints:
+
+1 <= coins.length <= 12
+1 <= coins[i] <= 231 - 1
+0 <= amount <= 104
+
+"""
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        if amount < 1:
+            return 0  #edge case
+        minCoinsDp = [float('inf')]*(amount + 1)
+        minCoinsDp[0] = 0 #base case
+
+        for i in range(1 , amount+1):  #DP
+            for coin in coins:
+                if coin <= i and minCoinsDp[i-coin] != float('inf'):
+                    minCoinsDp[i] = min(minCoinsDp[i], 1+minCoinsDp[i-coin])
+        #result
+        return -1 if minCoinsDp[amount] == float('inf') else minCoinsDp[amount]
